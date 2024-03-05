563b724a6913a682aaeb19e5e0e8a2fc46523576
umi2002 <jonathanhoang2002@gmail.com>
2024-03-05 13:22:35 -0500

    Implemented basic movement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerMovement : MonoBehaviour {
+  public float speed = 5f;
+  private Rigidbody2D rb;
+  private Vector2 movement;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  void Start() {}
+
+  void Update() {
+    float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+
+    movement = new Vector2(horizontal, vertical).normalized;
+  }
+
+  void FixedUpdate() {
+    rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+  }
 }
