82355f3c5a56dfc26c349b1082a538001c4691e5
MissChungus <telesanti58@hotmail.com>
2024-03-07 13:40:33 -0500

    flip character

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Déclaration des constantes
+    private static readonly Vector2 FlipRotation = new Vector3(0, 180);
+
     // Valeurs exposées
     [SerializeField]
     float MoveSpeed = 5.0f;
@@ -16,6 +19,7 @@ public class PlayerController : MonoBehaviour
     Animator _Anim { get; set; }
     bool mouvementEnabled {get; set; }
     Rigidbody2D _Rb { get; set; }
+    bool _Flipped { get; set; }
 
     void Awake() {
         _Anim = GetComponent<Animator>();
@@ -25,7 +29,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        _Flipped = false;
     }
 
     // Update is called once per frame
@@ -33,6 +37,7 @@ public class PlayerController : MonoBehaviour
     {
         var horizontal = mouvementEnabled ? Input.GetAxis("Horizontal") * MoveSpeed : 0;
         HorizontalMove(horizontal);
+        FlipCharacter(horizontal);
     }
 
     // Gère le mouvement horizontal
@@ -41,5 +46,19 @@ public class PlayerController : MonoBehaviour
         Debug.Log(horizontal);
         _Rb.velocity = new Vector2(horizontal, _Rb.velocity.y);
         //_Anim.SetFloat("MoveSpeed", Mathf.Abs(horizontal));
+    }
+
+    void FlipCharacter(float horizontal)
+    {
+        if (horizontal < 0 && !_Flipped)
+        {
+            _Flipped = true;
+            transform.Rotate(FlipRotation);
+        }
+        else if (horizontal > 0 && _Flipped)
+        {
+            _Flipped = false;
+            transform.Rotate(-FlipRotation);
+        }
     }
 }
