f07d366f37ac3efd5b73f8216dc28549cb2a2663
umi2002 <jonathanhoang2002@gmail.com>
2024-03-07 12:48:36 -0500

    Added implementation for raising on event on collision

--- Assets/Script/VFXChannelSO.cs
+++ Assets/Script/VFXChannelSO.cs
@@ -1,4 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu(menuName = "Events/VFX Channel")]
-public class VFXChannelSO : ScriptableObject { }
+public class VFXChannelSO : ScriptableObject
+{
+    public UnityAction<ParticleSystem[], Vector3> OnCollisionVFXRequested;
+
+    public void RaiseCollisionEvent(ParticleSystem[] particleSystems,
+                                    Vector3 position)
+    {
+        if (OnCollisionVFXRequested != null)
+        {
+            OnCollisionVFXRequested(particleSystems, position);
+        }
+    }
+}
