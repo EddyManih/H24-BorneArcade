dc4c03e1cb6d44aa4133d8b2ca3a4fd67e194993
umi2002 <jonathanhoang2002@gmail.com>
2024-03-28 14:20:38 -0400

    Changed how landing is detected

--- Assets/Script/vfx/VFXController.cs
+++ Assets/Script/vfx/VFXController.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class VFXController : MonoBehaviour {
   public VFXChannelSO vfxChannel;
-  public float groundCheckDistance = 0.1f;
-  public LayerMask groundLayer;
 
-  void Update() {
-    Vector2 origin = transform.position;
-    Vector2 direction = Vector2.down;
-    RaycastHit2D hit =
-        Physics2D.Raycast(origin, direction, groundCheckDistance, groundLayer);
-    if (hit.collider != null) {
-      vfxChannel.RaiseLandingEvent(hit.point);
+  private int _floorLayer = 3;
+
+  void OnCollisionEnter2D(Collision2D collision) {
+    if (collision.gameObject.layer == _floorLayer) {
+      Vector2 collisionCenter = GetCollisionCenter(collision);
+      vfxChannel.RaiseLandingEvent(collisionCenter);
     }
+  }
+
+  private Vector3 GetCollisionCenter(Collision2D collision) {
+    Vector2 center = Vector2.zero;
+    foreach (ContactPoint2D contact in collision.contacts) {
+      center += contact.point;
+    }
+    return center / collision.contacts.Length;
   }
 }
