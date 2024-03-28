aa15613d363ce3370a4ab9817d9a3b19a2a2e5bb
umi2002 <jonathanhoang2002@gmail.com>
2024-03-28 12:16:44 -0400

    Removed unused raise event

--- Assets/Script/vfx/VFXController.cs
+++ Assets/Script/vfx/VFXController.cs
@@ -15,18 +15,4 @@ public class VFXController : MonoBehaviour {
       vfxChannel.RaiseLandingEvent(hit.point);
     }
   }
-
-  public void OnCollisionEnter2D(Collision2D collision) {
-    Vector2 collisionCenter = GetCollisionCenter(collision);
-    vfxChannel.RaiseCollisionEvent(collisionCenter);
-  }
-
-  private Vector2 GetCollisionCenter(Collision2D collision) {
-    Vector2 center = Vector2.zero;
-    foreach (ContactPoint2D contact in collision.contacts) {
-      center += contact.point;
-    }
-    center /= collision.contacts.Length;
-    return center;
-  }
 }
