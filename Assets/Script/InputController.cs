ce05c8782066da79c35c47a6b3a501c0d740f7bb
Maxence Sigouin <maxence.sigouin@polymtl.ca>
2024-03-27 21:58:15 -0400

    Players can be controlled by different controllers but cannot choose which (first input is p1, second input is p2)

--- Assets/Script/InputController.cs
+++ Assets/Script/InputController.cs
@@ -1,76 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 public class InputController : MonoBehaviour
 {
-    [SerializeField] private InputAction HorizontalMovement;
-    [SerializeField] private InputAction JumpInput;
-    [SerializeField] private InputAction FallInput;
-    [SerializeField] private InputAction SlideToggle;
-    [SerializeField] private PlayerController playerController;
-    // Start is called before the first frame update
-    void Start()
+    private PlayerController playerController;
+    private PlayerInput playerInput;
+
+    private void Awake()
     {
-        // Logic when movement key is pressed
-        HorizontalMovement.performed += OnMovementPerformed;
-        // Logic when movement key is released
-        HorizontalMovement.canceled  += OnMovementPerformed;
-
-        //Logic when slide toggle is performed
-        SlideToggle.performed += OnSlideTogglePerformed;
-
-        JumpInput.performed += OnJumpPerformed;
-        // Logic when movement key is released
-        JumpInput.canceled  += OnJumpPerformed;
-
-        FallInput.performed += OnFallPerformed;
-        // Logic when movement key is released
-        FallInput.canceled  += OnFallPerformed;
-
+        playerInput = GetComponent<PlayerInput>();
+        PlayerController[] playerControllers = FindObjectsOfType<PlayerController>();
+        int index = playerInput.playerIndex;
+        playerController = playerControllers.FirstOrDefault(c => {
+            return c.GetPlayerIndex() == index;
+        });
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnMovementPerformed(InputAction.CallbackContext context)
+    public void OnMovementPerformed(InputAction.CallbackContext context)
     {
         float movement = context.ReadValue<float>();
         playerController.OnMovementInput(movement);
     }
-    private void OnJumpPerformed(InputAction.CallbackContext context)
+    public void OnJumpPerformed(InputAction.CallbackContext context)
     {
         bool jump = (context.ReadValue<float>() > 0.5f);
         playerController.OnJumpInput(jump);
     }
-    private void OnFallPerformed(InputAction.CallbackContext context)
+    public void OnFallPerformed(InputAction.CallbackContext context)
     {
        bool fall = (context.ReadValue<float>() > 0.5f);
         playerController.OnFallInput(fall);
     }
 
-    private void OnSlideTogglePerformed(InputAction.CallbackContext context)
+    public void OnSlideTogglePerformed(InputAction.CallbackContext context)
     {
         playerController.OnSlide();
-    }
-
-    private void OnEnable()
-    {
-        HorizontalMovement.Enable();
-        SlideToggle.Enable();
-        JumpInput.Enable();
-        FallInput.Enable();
-    }
-
-    private void OnDisable()
-    {
-        HorizontalMovement.Disable();
-        SlideToggle.Disable();
-        JumpInput.Enable();
-        FallInput.Enable();
     }
 }
