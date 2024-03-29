3e14c36dc33ff8caa53e548e36be72b441dee405
umi2002 <jonathanhoang2002@gmail.com>
2024-03-29 14:39:46 -0400

    Removed unused event

--- Assets/Script/vfx/VFXChannelSO.cs
+++ Assets/Script/vfx/VFXChannelSO.cs
@@ -6,7 +6,6 @@ public class VFXChannelSO : ScriptableObject
 {
   public UnityAction<Vector3> OnLandingVFXRequested;
   public UnityAction<Vector3> OnSlidingVFXRequested;
-  public UnityAction<Vector3> OnIsSlidingVFXRequested;
 
   public void RaiseLandingEvent(Vector3 position)
   {
@@ -21,14 +20,6 @@ public class VFXChannelSO : ScriptableObject
     if (OnSlidingVFXRequested != null)
     {
       OnSlidingVFXRequested.Invoke(position);
-    }
-  }
-
-  public void RaiseIsSlidingEvent(Vector3 position)
-  {
-    if (OnIsSlidingVFXRequested != null)
-    {
-      OnIsSlidingVFXRequested.Invoke(position);
     }
   }
 }
