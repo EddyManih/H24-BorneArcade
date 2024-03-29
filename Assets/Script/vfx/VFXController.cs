91b47a0ab3c36df05bb31532323a213071130838
umi2002 <jonathanhoang2002@gmail.com>
2024-03-29 14:43:12 -0400

    Formatting

--- Assets/Script/vfx/VFXController.cs
+++ Assets/Script/vfx/VFXController.cs
@@ -3,52 +3,52 @@ using UnityEngine;
 
 public class VFXController : MonoBehaviour
 {
-  public VFXChannelSO vfxChannel;
-  public PlayerController playerController;
+    public VFXChannelSO vfxChannel;
+    public PlayerController playerController;
 
-  private readonly int _floorLayer = 3;
+    private readonly int _floorLayer = 3;
 
-  void Start()
-  {
-    playerController = gameObject.GetComponent<PlayerController>();
-    PlayerController.OnSlideTriggered += OnSlideTriggered;
-    StartCoroutine(RaiseSlidingEvent());
-  }
-
-  void OnCollisionEnter2D(Collision2D collision)
-  {
-    if (collision.gameObject.layer == _floorLayer)
+    void Start()
     {
-      Vector2 collisionCenter = GetCollisionCenter(collision);
-      vfxChannel.RaiseLandingEvent(collisionCenter);
+        playerController = gameObject.GetComponent<PlayerController>();
+        PlayerController.OnSlideTriggered += OnSlideTriggered;
+        StartCoroutine(RaiseSlidingEvent());
     }
-  }
 
-  void OnSlideTriggered()
-  {
-    vfxChannel.RaiseSlidingEvent(gameObject.transform.position);
-  }
-
-  private Vector3 GetCollisionCenter(Collision2D collision)
-  {
-    Vector2 center = Vector2.zero;
-    foreach (ContactPoint2D contact in collision.contacts)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-      center += contact.point;
+        if (collision.gameObject.layer == _floorLayer)
+        {
+            Vector2 collisionCenter = GetCollisionCenter(collision);
+            vfxChannel.RaiseLandingEvent(collisionCenter);
+        }
     }
-    return center / collision.contacts.Length;
-  }
 
-  private IEnumerator RaiseSlidingEvent()
-  {
-    while (true)
+    void OnSlideTriggered()
     {
-      if (playerController._Sliding)
-      {
         vfxChannel.RaiseSlidingEvent(gameObject.transform.position);
-      }
-
-      yield return new WaitForSeconds(0.2f);
     }
-  }
+
+    private Vector3 GetCollisionCenter(Collision2D collision)
+    {
+        Vector2 center = Vector2.zero;
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            center += contact.point;
+        }
+        return center / collision.contacts.Length;
+    }
+
+    private IEnumerator RaiseSlidingEvent()
+    {
+        while (true)
+        {
+            if (playerController._Sliding)
+            {
+                vfxChannel.RaiseSlidingEvent(gameObject.transform.position);
+            }
+
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
 }
