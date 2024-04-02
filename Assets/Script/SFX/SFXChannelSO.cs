8ae6c02d3300e2e7050c6194af389f0d03ceb2d1
umi2002 <jonathanhoang2002@gmail.com>
2024-04-02 19:16:14 -0400

    Formatting

--- Assets/Script/SFX/SFXChannelSO.cs
+++ Assets/Script/SFX/SFXChannelSO.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [CreateAssetMenu(menuName = "Events/SFX Channel")]
-public class SFXChannelSO : ScriptableObject {
-  public UnityAction OnPlaySFXRequested;
-  public AudioSource clip;
+public class SFXChannelSO : ScriptableObject
+{
+    public UnityAction OnPlaySFXRequested;
+    public AudioSource clip;
 
-  public void RaisePlaySFXEvent() {
-    if (OnPlaySFXRequested != null) {
-      OnPlaySFXRequested.Invoke();
+    public void RaisePlaySFXEvent()
+    {
+        if (OnPlaySFXRequested != null)
+        {
+            OnPlaySFXRequested.Invoke();
+        }
     }
-  }
 }
