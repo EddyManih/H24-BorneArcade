005fc1cdcffe447dfae0a2532f6eef60423584c5
Maxence Sigouin <maxence.sigouin@polymtl.ca>
2024-04-03 21:09:21 -0400

    Wait 3s to show end menu

--- Assets/Script/ArenaController.cs
+++ Assets/Script/ArenaController.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class ArenaController : MonoBehaviour
 {
     PlayerController[] playerControllers;
+    [SerializeField] GameObject endMenu;
+    [SerializeField] GameObject replayButton;
 
     void Start()
     {
@@ -17,10 +20,23 @@ public class ArenaController : MonoBehaviour
 
     void EndGame()
     {
+        StartCoroutine(EndGameCoroutine());
+    }
+
+    IEnumerator EndGameCoroutine()
+    {
         foreach (PlayerController playerController in playerControllers)
         {
             if (playerController) playerController.DisableMovement();
         }
+
+        foreach (GameObject input in GameObject.FindGameObjectsWithTag("Input"))
+        {
+            input.SetActive(false);
+        }
+
+        yield return new WaitForSeconds(3.0f);
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
