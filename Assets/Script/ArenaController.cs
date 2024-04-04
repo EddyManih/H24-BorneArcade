1bef9399be41bc2455dacf3c6785d11efe33fc16
Maxence Sigouin <maxence.sigouin@polymtl.ca>
2024-04-03 21:15:26 -0400

    Can adjust waiting time at the end of the game in editor

--- Assets/Script/ArenaController.cs
+++ Assets/Script/ArenaController.cs
@@ -5,8 +5,7 @@ using System.Collections;
 public class ArenaController : MonoBehaviour
 {
     PlayerController[] playerControllers;
-    [SerializeField] GameObject endMenu;
-    [SerializeField] GameObject replayButton;
+    [SerializeField] private float waitForMenuTime;
 
     void Start()
     {
@@ -35,7 +34,7 @@ public class ArenaController : MonoBehaviour
             input.SetActive(false);
         }
 
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(waitForMenuTime);
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
